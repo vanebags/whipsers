dcf86c7f9618bf2fbc331e8fc296727ab9d63b33
Vanessa Bagshaw <vanessabagshaw@Vanessas-MacBook-Air.local>
2015-06-26 18:23:04 -0400

    wip

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 		<div class="wrapper clearfix">
 			<h1 id="logo">Whispers of The Oracle</h1>
 			<ul id="nav">
-				<li><a href="home.html" class="active">Home</a></li>
-				<li><a href="readers.html">Readers</a></li>
-				<li><a href="pr.html">Personal Readings</a></li>
-				<li><a href="book.html">Book</a></li>
-				<li><a href="blog.html">Blog</a></li>
+				<li><a href="<?php echo TPL_DIR ?>home.html" class="active">Home</a></li>
+				<li><a href="<?php echo TPL_DIR ?>readers.html">Readers</a></li>
+				<li><a href="<?php echo TPL_DIR ?>pr.html">Personal Readings</a></li>
+				<li><a href="<?php echo TPL_DIR ?>book.html">Book</a></li>
+				
 			</ul>
 		</div>
 	</div>
@@ -17,10 +17,10 @@
 			<div id="hero-wrapper">
 				<div class="inner">
 					<ul id="hero" class="clearfix">
-						<li><img src="<?php echo TPL_DIR ?>/img/1.jpg" alt=""></li>
-						<li><img src="<?php echo TPL_DIR ?>/img/2.jpg" alt=""></li>
-						<li><img src="<?php echo TPL_DIR ?>/img/3.jpg" alt=""></li>
-						<li><img src="<?php echo TPL_DIR ?>/img/4.jpg" alt=""></li>
+						<li><img src="<?php echo TPL_DIR ?>/img/1.jpeg" alt=""></li>
+						<li><img src="<?php echo TPL_DIR ?>/img/2.jpeg" alt=""></li>
+						<li><img src="<?php echo TPL_DIR ?>/img/3.jpeg" alt=""></li>
+						<li><img src="<?php echo TPL_DIR ?>/img/4.jpeg" alt=""></li>
 					</ul>
 				</div>
 
@@ -33,7 +33,10 @@
 			</div>
 			<div id="home-ctas" class="clearfix">
 				<p class="pane">
-					Whispers of the Oracle, LLC, is a company devoted to bringing insight and enlightenment to the questions of your heart, soul and mind. In the famed tradition of the “Oracle of Delphi” our dedicated readers use a variety of divinatory tools and talents.  Our service provides accurate and caring guidance that will help you move through the complex, and sometimes confusing world, more comfortably. Whispers of the Oracle is the Voice and the Light that helps you find answers to these questions and more.  We also urge you to take a look at our “Underground” location page for information on how to visit our expert readers in person while in the great city of Atlanta, GA. otherwise; use this site to call us for excellent over the phone consultations.
+					Whispers of the Oracle, LLC, is a company devoted to bringing insight and enlightenment to the questions of your heart, soul and mind. In the famed tradition of the “Oracle of Delphi” our dedicated readers use a variety of divinatory tools and talents.  Our service provides accurate and caring guidance that will help you move through the complex, and sometimes confusing world, more comfortably.
+		
+					<br>Whispers of the Oracle is the Voice and the Light that helps you find answers to these questions and more.  We also urge you to take a look at our “Underground” location page for information on how to visit our expert readers in person while in the great city of Atlanta, GA. otherwise; use this site to call us for excellent over the phone consultations.
+					</br>
 				</p>
 
 				<ul id="fromTheBlog" class="pane">
