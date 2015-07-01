1f9e1a8fd5830ea81349dd10b17a6ba13e992279
Vanessa Bagshaw <vanessabagshaw@Vanessas-MacBook-Air.local>
2015-06-30 22:50:17 -0400

    changed some header styles and p h2 colors

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 	<div id="main">
-		<div id="content">
+		<div id="content" class="clearfix">
 			<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 				<h1><?php the_title(); ?></h1>
 				<?php the_content(); ?>
--- page.php
+++ page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div id="main" class="wrapper">
-	<div id="content">
+	<div id="content" class="clearfix">
 		<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 			<?php the_content(); ?>
 		<?php endwhile; endif; ?>
