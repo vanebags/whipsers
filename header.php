c18717a3dffc7eb64956e28bd6c007b43ad8ad24
Vanessa Bagshaw <vanessabagshaw@Vanessas-MacBook-Air.local>
2015-06-30 23:03:53 -0400

    wip

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
 <body <?php body_class(); ?>>
 
 <div id="header" class="clearfix">
+	<a id="phone" href="tel:888-505-2133"><i class="fa fa-phone"></i>&nbsp; 888-505-2133</a>
 	<div class="wrapper clearfix">
 		<a id="logo" href="<?php bloginfo('url') ?>"><h1>Whispers of The Oracle</h1></a>
 		<?php wp_nav_menu(array(
